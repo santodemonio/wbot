22fb96acb6ce440e1f650003cc8b1cedc3b962bd
Arunraj <santomaybenow@gmail.com>
2025-01-30 15:13:04 +0530

    Update start.sh script

--- start.sh
+++ start.sh
@@ -6,10 +6,5 @@ Xvfb :99 -screen 0 1024x768x24 &
 # Export display environment variable
 export DISPLAY=:99
 
-# Ensure the .Xauthority file is in place
-xauth generate :99 . trusted
-xauth add $(xauth list)
-chmod 600 ./.Xauthority
-
 # Start the Flask app using Gunicorn
 exec gunicorn app:app
